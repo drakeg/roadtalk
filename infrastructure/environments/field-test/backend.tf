7b1cc3c1ab1db17ba18b550dd8457eba21cafc96
Greg Drake <greg@madmallards.com>
2026-07-12 18:41:14 -0400

    S01-D09: make the S3 backend Terraform 1.15-valid

--- infrastructure/environments/field-test/backend.tf
+++ infrastructure/environments/field-test/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket       = "roadtalk-state-configure-me"
+    key          = "field-test/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
