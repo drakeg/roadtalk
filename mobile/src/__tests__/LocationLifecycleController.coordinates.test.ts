9a5283b2194008a4ebea6f6d21130252ebc442ce
Greg Drake <greg@madmallards.com>
2026-08-28 22:09:51 -0400

    Remove accidental placeholder test

--- mobile/src/__tests__/LocationLifecycleController.coordinates.test.ts
+++
@@ -1,5 +0,0 @@
-import { LocationLifecycleController } from "../location/LocationLifecycleController";
-
-test("owner location snapshot exposes local coordinates for map rendering", () => {
-  expect(LocationLifecycleController).toBeDefined();
-});
